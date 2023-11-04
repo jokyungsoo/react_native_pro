ae4405d658d99d80f189ab548c6ec0a488cbe93f
PJO <87128671+jokyungsoo@users.noreply.github.com>
2023-11-04 21:43:27 +0900

    Hook

--- projects_01/first-my-app/CustomHook..js
+++ projects_01/first-my-app/CustomHook..js
@@ -1,49 +1,60 @@
 import React, { useState } from 'react';
 import { View, TextInput, Button } from 'react-native';
 
+const InputBox = (props) => {
+    return (
+        /*변수명을 불러와야댐. */
+        <View style={{ flexDirection: 'row' }}>
+            <TextInput
+                value={props.value}
+                placeholder={props.placeholder}
+                onChangeText={props.onChangeText}
+                style={{ borderBottomWidth: 1, width: 200 }}
+            />
+            <Button title="reset" onPress={props.onReset} />
+        </View>
+    );
+};
+
+/* initialValue 는 입력값 */
+const useInput = (initialValue) => {
+    const [value, setValue] = useState(initialValue);
+    const resetValue = () => setValue(initialValue);
+
+    return {
+        value,
+        setValue,
+        resetValue,
+    };
+};
+
 const CustomHook = () => {
-    const [name, setName] = useState('');
-    const [age, setAge] = useState('');
-    const [city, setCity] = useState('');
+    /*useInput의 기본값을 빈문자열로 정의해줌 */
+    const { value: name, setValue: setName, resetValue: resetName } = useInput('');
+    const { value: age, setValue: setAge, resetValue: resetAge } = useInput('');
+    const { value: city, setValue: setCity, resetValue: resetCity } = useInput('');
 
     return (
         <View>
-            <View style={{ flexDirection: 'row' }}>
-                <TextInput
-                    /* 아래 두 라인은 같은 기능을하는 코드이다. 
-                    onChangeText={setName}는 텍스트가 입력되었을때 setName을 호출하여 텍스트를 화면상 출력해준다.
-                    onchangeText={(v) => {setName(v);}}는 텍스트(v)가 입력시 setName을 호출하여 텍스트를 화면상에 출력해준다. */
-                    value={name}
-                    placeholder="name"
-                    onChangeText={setName}
-                    // onchangeText={(v) => {setName(v);}}
-                    style={{ borderBottomWidth: 1, width: 200 }}
-                />
-                {/* 아래 버튼의 setName('')부분은 괄호안에 false가 와도 작동은하지만 계연성이 없으므로 ""빈문자열을 넣어줬다. */}
-                <Button title="reset" onPress={() => setName('')} />
-            </View>
-
-            <View style={{ flexDirection: 'row' }}>
-                <TextInput
-                    value={age}
-                    onChangeText={setAge}
-                    placeholder="age"
-                    style={{ borderBottomWidth: 1, width: 200 }}
-                />
-                <Button title="reset" onPress={() => setAge('')} />
-            </View>
-
-            <View style={{ flexDirection: 'row' }}>
-                <TextInput
-                    value={city}
-                    onChangeText={setCity}
-                    placeholder="city"
-                    style={{ borderBottomWidth: 1, width: 200 }}
-                />
-                <Button title="reset" onPress={() => setAge('')} />
-            </View>
+            <InputBox value={name} placeholder="name" onChangeText={setName} onReset={resetName} />
+            <InputBox value={age} placeholder="age" onChangeText={setAge} onReset={resetAge} />
+            <InputBox value={city} placeholder="city" onChangeText={setCity} onReset={resetCity} />
         </View>
     );
 };
 
 export default CustomHook;
+// /* <View style={{ flexDirection: 'row' }}> */
+//     /* <TextInput */
+//         /* /* 아래 두 라인은 같은 기능을하는 코드이다.  */
+//         /* onChangeText={setName}는 텍스트가 입력되었을때 setName을 호출하여 텍스트를 화면상 출력해준다. */}
+//         /* onchangeText={(v) => {setName(v);}}는 텍스트(v)가 입력시 setName을 호출하여 텍스트를 화면상에 출력해준다. */ */}
+//         /* // value={name} */
+//         /* // placeholder="name" */
+//         /* // onChangeText={setName} */
+//         /* // onchangeText={(v) => {setName(v);}} */
+//         /* style={{ borderBottomWidth: 1, width: 200 }} */
+//     /* /> */
+//     /* 아래 버튼의 setName('')부분은 괄호안에 false가 와도 작동은하지만 계연성이 없으므로 ""빈문자열을 넣어줬다. */}
+//     /* <Button title="reset" onPress={() => setName('')} /> */
+// /* </View> */
---
+++ projects_01/first-my-app/src/Header.js
@@ -0,0 +1,47 @@
+// yarn add react-native-iphone-x-helper : iphon-x
+import { StyleSheet, View, Text } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+/* 아이콘은 모두 함수로 뿌려준다. */
+const IconButton = (props) => {
+    return (
+        <View style={styles.icon_top}>
+            <Ionicons name={props.name} size={24} color="black" />
+        </View>
+    );
+};
+
+export default () => {
+    return (
+        <View style={styles.headerContainer}>
+            <Text style={styles.title}>친구</Text>
+            <View style={styles.headerContainer_right}>
+                <IconButton name="search-outline" />
+                <IconButton name="person-add-outline" />
+                <IconButton name="md-musical-notes-outline" />
+                <IconButton name="ios-settings-outline" />
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        // backgroundColor: 'grey',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+    },
+    headerContainer_right: {
+        flexDirection: 'row',
+    },
+
+    title: {
+        fontSize: 22,
+        fontWeight: 'bold',
+    },
+    icon_top: {
+        paddingHorizontal: 7,
+    },
+});
