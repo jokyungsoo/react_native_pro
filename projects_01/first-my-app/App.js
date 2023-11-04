9d550805ccf3b05b6944c8feaf02ac43f34a7f95
PJO <87128671+jokyungsoo@users.noreply.github.com>
2023-11-05 00:12:48 +0900

    Update App.js

--- projects_01/first-my-app/App.js
+++ projects_01/first-my-app/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import { useState } from 'react';
-import { Platform, SafeAreaView, StyleSheet, Text, TextInput, View, Button } from 'react-native';
-import Header from './src/Header';
+import React, { useState } from 'react';
+
+import { Platform, StyleSheet, Text, TextInput, View, Button, Image } from 'react-native';
+//iphon-x
 import { getBottomSpace, getStatusBarHeight } from 'react-native-iphone-x-helper';
+//yarn add react-native-safe-area-context를 해줘야 사용가능함.
+// import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+//import file
+
+import Header from './src/Header';
+import MyProfile from './src/MyProfile';
+import { myProfile } from './src/date';
+import Margin from './src/Margin';
 
 const StatusBarHeight = getStatusBarHeight(true);
 const BottomSpace = getBottomSpace();
@@ -11,8 +19,17 @@ console.log(`${Platform.OS}: ${StatusBarHeight}, ${BottomSpace}`);
 
 export default function APP() {
     return (
+        // <SafeAreaProvider>
+        //     <SafeAreaView style={styles.container} edges={['right', 'left']}>
+        //         <Header />
+        //     </SafeAreaView>
+        // </SafeAreaProvider>
         <View style={styles.container}>
             <Header />
+            {/* Margin 함수를 만들어주고 호출함 */}
+            <Margin height={10} />
+            {/* MyProfile 함수를 만들어주고 호출함, date.myProfile 함수를 호출함 */}
+            <MyProfile uri={myProfile.uri} name={myProfile.name} introduction={myProfile.introduction} />
         </View>
     );
 }
@@ -23,6 +40,7 @@ const styles = StyleSheet.create({
         paddingBottom: BottomSpace,
         paddingTop: StatusBarHeight,
         backgroundColor: '#fff',
+
         // alignItems: 'center',
         // justifyContent: 'flex-end',
     },
