0faf2ee48f7e3bae0c43fe39d2ff30274b75dd9d
PJO <87128671+jokyungsoo@users.noreply.github.com>
2023-11-05 00:34:14 +0900

    Create Division.js

---
+++ projects_01/first-my-app/src/Division.js
@@ -0,0 +1,7 @@
+import { View } from 'react-native';
+
+const Division = () => {
+    return <View style={{ width: '100%', height: 0.5, backgroundColor: 'black', opacity: 0.3 }} />;
+};
+
+export default Division;
